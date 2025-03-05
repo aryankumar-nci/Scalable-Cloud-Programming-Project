b232b536b14e20e1279ca925995837abe8c2a912
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-05 22:09:17 +0000

    categories_navbar+

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from .models import Category
 # Create your views here.
 
 def store(request):
     
     return render(request, 'store/store.html')
+
+# nav bar categories, will be availible in all pages.
+def categories(request):
+    
+    all_categories = Category.objects.all() 
+    
+    return {'all_categories': all_categories}
+
 
