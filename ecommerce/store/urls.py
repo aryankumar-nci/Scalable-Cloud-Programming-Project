e788ffffee2b8c37e50cd4519517288084dd3084
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-05 22:45:07 +0000

    redirect link to specific product+

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
      
      path('',views.store, name='store'),
+     
+     # this will redirect to the specific product.
+     path('product/<slug:slug>/', views.product_info, name='product-info'),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .models import Category, Product
 
+from django.shortcuts import get_object_or_404
+
 def store(request):
     #brings all product from db to the frontpage
     all_products = Product.objects.all()
@@ -16,5 +18,16 @@ def categories(request):
     all_categories = Category.objects.all() 
     
     return {'all_categories': all_categories}
+
+
+#get individual product from db
+
+def product_info(request,slug):
+    
+    product = get_object_or_404(Product,slug=slug)
+    
+    context = {'product': product}
+
+    return render(request, 'store/product-info.html', context)
 
 
