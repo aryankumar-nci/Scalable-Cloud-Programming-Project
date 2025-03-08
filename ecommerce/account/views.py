6e76a5fa7e50178e0aa9a7e3b13a4903e6eef0a1
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-08 14:29:43 +0000

    email verification1 added (63)

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 
 from .forms import CreateUserForm
 
+from django.contrib.sites.shortcuts import get_current_site
+from .token import user_tokenizer_generate
+
+
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+
+
+
 def register(request):
     
     form = CreateUserForm()
@@ -12,9 +23,29 @@ def register(request):
         
         if form.is_valid():
             
-            form.save()
+            user = form.save()
             
-            return redirect('store')
+            user.is_active = False
+            
+            user.save()
+            
+            #email verification setup (template)
+            
+            current_site = get_current_site(request)
+            
+            subject = 'Account verification email'
+            
+            message = render_to_string('account/registration/email-verification.html',{
+                
+                'user':user, 
+                'domain':current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':user_tokenizer_generate.make_token(user),
+                
+                
+                         })
+            
+            return redirect('email-verification-sent')
         
     context = {'form':form} 
            
