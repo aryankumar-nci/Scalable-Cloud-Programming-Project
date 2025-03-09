ff99a09caa64a81a78a4bf87fcdd3c723cf09dd7
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-09 20:44:21 +0000

    order and order items added and migration done (99)

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from store.models import Product
+
 class ShippingAddress(models.Model):
     
     full_name = models.CharField(max_length=300)
@@ -35,3 +37,53 @@ class ShippingAddress(models.Model):
     def __str__(self):
         
         return 'Shipping Address - ' + str(self.id)
+    
+    
+#Order models--------------------------------------------------------
+
+class Order(models.Model):
+    
+    full_name = models.CharField(max_length=300)
+    
+    email = models.EmailField(max_length=255)
+    
+    shipping_address = models.TextField(max_length=10000)
+    
+    #amount paid
+    
+    amount_paid = models.DecimalField(max_digits=8,decimal_places=2)
+    
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    
+    #Forign-key 
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        
+        return '0rder - #' + str(self.id)
+
+
+#Order item models----------------
+
+class OrderItem(models.Model):
+    
+    #forign key->
+    
+    order = models.ForeignKey(Order,on_delete=models.CASCADE, null=True)
+    
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+    
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
+    
+    def __str__(self):
+        
+        return '0rder item - #' + str(self.id)
+    
+    
+      
