b66f3a4e8429ec844acd0f09361a571ad7554c17
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-09 06:14:58 +0000

    login functionality tested ok (71)

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     
     #Login and Logout URL
     
-    path('my-login', views.my_login, name='my_login'),
+    path('my-login', views.my_login, name='my-login'),
     
     path('dashboard', views.dashboard,name='dashboard'),
         
