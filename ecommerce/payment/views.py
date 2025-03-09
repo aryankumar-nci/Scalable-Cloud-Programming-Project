2070c41190e5ba2bf271addde8a4c3642bd318f7
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-09 20:17:35 +0000

    checkout functionality added (98)

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 
+from . models import ShippingAddress
+
 # Create your views here.
 
 def checkout(request):
     
-    return render(request,'payment/checkout.html')
+    #users with account will be pre-filled the shipping details.
+    
+    if request.user.is_authenticated:
+        
+        try:
+            #authenticated user WITH shipping information.
+            
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            
+            context = {'shipping': shipping_address}
+            
+            return render(request,'payment/checkout.html',context=context)
+        
+        except:
+            
+            #authenticated user with No shipping information.
+            return render(request,'payment/checkout.html')
+    else:   
+        #for the guest users
+        return render(request,'payment/checkout.html')
 
 def payment_success(request):
     
