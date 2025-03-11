c97bdf9d8d0bf5bb51f8ab09693e8f31ab19a395
Aryan Kumar <x23310618@student.ncirl.ie>
2025-03-11 10:59:48 +0000

    settings.py changed for deployment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y8$hu*zy(w*=*$svwu7ki2_q6ah@xadu+vx*5%t2a!4vyg3#_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
+#CSRF_TRUSTED_ORIGINS = ['']
 
 
 # Application definition
